b979a9a5df77c9600b1ca82f54a4d18dc676996a
= <=>
2019-02-12 16:26:06 +0300

    Play Sound Effect

--- PlaySoundEffect/ViewController.swift
+++ PlaySoundEffect/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+    
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func handlePlayButton(_ sender: Any) {
+        playSound()
+    }
+    
+    fileprivate func playSound() {
+        guard let url = Bundle.main.url(forResource: "resultVCSoundEffect", withExtension: "wav") else { return }
+        let delay : TimeInterval = 5.0
+        do {
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            guard let avPlayer = player else { return }
+            let now : TimeInterval = avPlayer.deviceCurrentTime
+            avPlayer.play(atTime: now + delay)
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
